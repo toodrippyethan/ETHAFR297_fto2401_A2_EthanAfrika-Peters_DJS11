9b6042a6a02b4b96eff3377200f4b853f3786cda
Ethan <afrikapetersek@gmail.com>
2024-06-19 01:14:14 +0200

    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <div className="nav">Header</div>
+  return <div className="navbar">
+    <div className="links">
+    <Link to="/">SignUp</Link>
+    <Link to="/podcasts">Podcasts</Link>
+    <Link to="/start-a-podcast">Start a Podcast</Link>
+    <Link to="/profile">Profile</Link>
+    </div>
+  </div>;
 }
 
 export default Header;
