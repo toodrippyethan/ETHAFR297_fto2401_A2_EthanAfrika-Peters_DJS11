4308304d8442a8df9eecf2a1d4cf4bfad5d9cc90
Ethan <afrikapetersek@gmail.com>
2024-06-19 01:08:26 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import SignUpPage from './pages/SignUp';
 
 function App() {
   return (
-    <div className="App"><Header /></div>
+    <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<SignUpPage />} />
+          </Routes>
+        </Router>
+    </div>
   );
 }
 
