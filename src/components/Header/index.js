3630003ca99854dd58f0f5fa927253bb497b25f0
Ethan <afrikapetersek@gmail.com>
2024-06-19 19:25:59 +0200

    SignUp Pt1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,26 +3,26 @@ import "./styles.css";
 import { Link, useLocation } from "react-router-dom";
 
 function Header() {
-  const location = useLocation();
-  const currentPath = location.pathname;
+const location = useLocation();
+const currentPath = location.pathname;
 
-  return <div className="navbar">
-    <div className="links">
-    <div className="gradient"></div>
-    <Link to="/" className={currentPath == "/" ? "active" : ""}>
-    SignUp
-    </Link>
-    <Link to="/podcasts" className={currentPath == "/podcasts" ? "active" : ""}>
-    Podcasts
-    </Link>
-    <Link to="/start-a-podcast" className={currentPath == "/start-a-podcast" ? "active" : ""}>
-    Start a Podcast
-    </Link>
-    <Link to="/profile" className={currentPath == "/profile" ? "active" : ""}>
-    Profile
-    </Link>
-    </div>
+return <div className="navbar">
+<div className="links">
+<div className="gradient"></div>
+<Link to="/" className={currentPath == "/" ? "active" : ""}>
+SignUp
+</Link>
+<Link to="/podcasts" className={currentPath == "/podcasts" ? "active" : ""}>
+Podcasts
+</Link>
+<Link to="/start-a-podcast" className={currentPath == "/start-a-podcast" ? "active" : ""}>
+Start a Podcast
+</Link>
+<Link to="/profile" className={currentPath == "/profile" ? "active" : ""}>
+Profile
+</Link>
+</div>
+
   </div>;
 }
-
 export default Header;
