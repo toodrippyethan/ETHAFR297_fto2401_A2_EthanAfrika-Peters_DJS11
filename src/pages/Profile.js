749b82e4a04af376b1463aeb49ceaa8a6447379f
Ethan <afrikapetersek@gmail.com>
2024-06-20 18:39:34 +0200

    Profile update

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import Header from '../components/common/Header';
 import Button from '../components/common/Button';
@@ -6,32 +6,34 @@ import { signOut } from 'firebase/auth';
 import { toast } from 'react-toastify';
 import { auth } from '../firebase';
 
-function Profile () {
-    const user = useSelector((state) => state.user.user);
-   
-    console.log("My User", user);
-   if (!user) {
-    return <p>Loading...</p>
-   }
-   const handleLogout = () => {
-    signOut(auth)
-      .then(() => {
-        toast.success("User Logged Out!");
-      })
-      .catch((error) => {
-        // An error happened.
-        toast.error(error.message);
-      });
+function Profile() {
+  const user = useSelector((state) => state.user.user);
+
+  // Loading state while user is fetched
+  if (!user) {
+    return <p>Loading...</p>; // You can replace this with a loading spinner or animation
+  }
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      toast.success("User Logged Out!");
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
-    return (
-        <div>
-            <Header />
-            <h1>{user.name}</h1>
-            <h1>{user.email}</h1>
-            <h1>{user.uid}</h1>
-            <Button text={"Logout"} onClick={handleLogout} />
-        </div>
-    );
+  return (
+    <div style={{ padding: '20px' }}>
+      <Header />
+      <div style={{ maxWidth: '600px', margin: 'auto', textAlign: 'center' }}>
+        <h1 style={{ marginBottom: '10px' }}>Welcome, {user.name}</h1>
+        <p style={{ marginBottom: '10px' }}>Email: {user.email}</p>
+        <p style={{ marginBottom: '20px' }}>UID: {user.uid}</p>
+        <Button text="Logout" onClick={handleLogout} />
+      </div>
+    </div>
+  );
 }
-export default Profile
+
+export default Profile;
